1042d42a3c3785c4d580f467abf72ec13ad69c5c
zubairalam1997 <zubairalamraza@gmail.com>
2024-01-20 17:23:04 +0530

    nav update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -95,7 +95,7 @@ const Navbar = () => {
                 <a href="#" className="-m-2 flex items-center p-2">
                   <img
                     src="img/indiaflag.png"
-                    alt=""
+                    alt="flg img"
                     className="block h-auto w-5 flex-shrink-0"
                   />
                   <span className="ml-3 block text-base font-medium text-gray-900" style={{ color: mode === 'dark' ? 'white' : '', }}>INDIA</span>
